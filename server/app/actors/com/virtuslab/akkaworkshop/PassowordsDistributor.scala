7842d8ab3f1f92c97264d97b474c1cfb73d16631
Piotr Kukielka <piotr.kukielka@gmail.com>
2014-09-23 22:55:04 +0200

    Add server part

---
+++ server/app/actors/com/virtuslab/akkaworkshop/PassowordsDistributor.scala
@@ -0,0 +1,94 @@
+package com.virtuslab.akkaworkshop
+
+import akka.actor.Actor
+import akka.actor.Props
+import akka.event.Logging
+import scala.util.Random
+import org.apache.commons.codec.binary.Base64
+
+object PasswordsDistributor {
+  type Token = String
+}
+
+import PasswordsDistributor._
+
+sealed trait PDMessageRequest
+case class Register(name: String) extends PDMessageResponse
+case class SendMeEncryptedPassword(token: Token) extends PDMessageResponse
+case class ValidateDecodedPassword(token: Token,
+                                   encryptedPassword: String,
+                                   decryptedPassword: String) extends PDMessageResponse
+
+sealed trait PDMessageResponse
+case class Registered(token: Token)
+case class EncryptedPassword(encryptedPassword: String) extends PDMessageResponse
+case class PasswordCorrect(decryptedPassword: String) extends PDMessageResponse
+case class PasswordIncorrect(decryptedPassword: String) extends PDMessageResponse
+
+sealed trait PDMessageInternal
+case object SendMeStatistics extends PDMessageInternal
+case class Statistics(clients: Seq[Client])
+
+class Client(name: String) {
+  var lastActionTimestamp = 0L
+  var passwordsRequested = 0
+  var passwordsDecrypted = 0
+  var passwordsInvalid = 0
+
+  def timestamp = System.currentTimeMillis / 1000
+
+  def isActive = (timestamp - lastActionTimestamp) < 60
+
+  def updateTimestamp() {
+    lastActionTimestamp = timestamp
+  }
+}
+
+class PasswordsDistributor extends Actor {
+  val log = Logging(context.system, this)
+  var clients = scala.collection.immutable.HashMap.empty[Token, Client]
+
+  def randomString(n: Int) = new String(Seq.fill(n)(Random.nextPrintableChar).toArray)
+
+  def receive = {
+    case Register(name) =>
+      val token = randomString(32)
+      val client = new Client(name)
+      client.updateTimestamp()
+      clients = clients + ((token, client))
+      sender ! Registered(token)
+
+    case SendMeEncryptedPassword(token) =>
+      def encrypt(password: String) = new String(Base64.encodeBase64(password.getBytes))
+
+      clients.get(token) foreach {
+        client =>
+          val decrypted = randomString(8 + Random.nextInt(8))
+          val encrypted = encrypt(encrypt(encrypt(decrypted)))
+          client.updateTimestamp()
+          client.passwordsRequested += 1
+          sender ! EncryptedPassword(encrypted)
+      }
+
+    case ValidateDecodedPassword(token, encrypted, decrypted) =>
+      def decrypt(password: String) = new String(Base64.decodeBase64(password.getBytes))
+
+      clients.get(token) foreach {
+        client =>
+          client.updateTimestamp()
+
+          if (decrypted == decrypt(decrypt(decrypt(encrypted)))) {
+            client.passwordsDecrypted += 1
+            sender ! PasswordCorrect(decrypted)
+          }
+          else {
+            client.passwordsInvalid += 1
+            sender ! PasswordIncorrect(decrypted)
+          }
+      }
+
+    case SendMeStatistics =>
+      clients = clients filter { case (t, c) => c.isActive }
+      sender ! Statistics(clients.values.toSeq)
+  }
+}
